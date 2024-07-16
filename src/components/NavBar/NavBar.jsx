aa278bab73936feca166ed0bfe317a6b199c4718
saoirse byrne <saoirsebyrne98@gmail.com>
2024-07-16 16:57:31 +0930

    saving

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,33 +8,14 @@ const NavBar = ({ handleSignout }) => {
   return (
     <>
       {user ? (
-<<<<<<< HEAD
-        <nav class="bg-gray-100">
-          <div class=" px-4 py-2 mx-auto border">
-            <div class="flex">
-              <div>
-                <Link to="/books"> Home </Link>
-                <Link to="/reviews"> Reviews </Link>
-                </div>
-
-                <Link to="" onClick={handleSignout}> Sign Out </Link>
-              </div>
-          </div>
-=======
         <nav>
-          <ul>
-            <li><Link to="/">Home</Link>
-            <li><Link to="/books">Books</Link></li>
-            <li><Link to="/reviews">Reviews</Link></li>
-            <li><Link to="/members">Members</Link></li>
-            </li>
-            <li>
-              <Link to="" onClick={handleSignout}>
-                Sign Out
-              </Link>
-            </li>
-          </ul>
->>>>>>> aa09a4897ec7a56b1f3a36bef9e3ef310e0989b2
+
+            <Link to="/">Home</Link>
+            <Link to="/books">Books</Link>
+            <Link to="/reviews">Reviews</Link>
+            <Link to="/members">Members</Link>
+            <Link to="" onClick={handleSignout}>Sign Out</Link>
+
         </nav>
       ) : (
         <nav class="bg-gray-100">
